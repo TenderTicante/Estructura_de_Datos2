9109363ebf832952735fb8a4b84f28bb53a93312
Tender Ticante <tenderticante@gmail.com>
2015-09-20 17:06:29 -0500

    Comenzando calculadora

---
+++ calculadora/funciones.c
@@ -0,0 +1,43 @@
+nclude "funciones.h"
+
+
+
+
+
+float suma (float num1, float num2) 
+
+{
+
+	return  num1 + num2;
+
+}
+
+
+
+float resta (float num1, float num2) 
+
+{
+
+	return  num1 - num2;
+
+}
+
+
+
+float multiplicacion (float num1, float num2) 
+
+{
+
+	return  num1 * num2;
+
+}
+
+
+
+float division (float num1, float num2) 
+
+{
+
+	return  num1 / num2;
+
+}
